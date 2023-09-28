956fb36f1853f955130f87a72cda7f992f5b3d94
ANNEMARIE05 <annemarieagbonou@gmail.com>
2023-09-28 17:07:00 +0000

    front

--- src/assets/pages/inscription.jsx
+++ src/assets/pages/inscription.jsx
@@ -1,105 +1,72 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from 'axios';
 import "../css/inscription.css"
 
 
 function Register() {
-  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: '',
+    contact: '',
+    email: '',
+    password: '',
 
-  // State pour recevoir la valeur de notre inputs
-  const [inputValue, setInputValue] = useState({
-    username: "",
-    email: "",
-    password: "",
-    contact: "",
   });
-  const { username, email, password, contact } = inputValue;
 
-  // Event Pour remplire les inputs
-  const handleOnChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    // Fonction de mise ajour des valeur a chaque saisir
-    setInputValue({ ...inputValue, [name]: value });
+    setFormData({ ...formData, [name]: value });
   };
 
-  // Cretation des sesssion de message
-  const handleError = (err) =>
-    toast.error(err, {
-      position: "bottom-left",
-    });
-  const handleSuccess = (msg) =>
-    toast.success(msg, {
-      position: "bottom-right",
-    });
-
-  // Creation du bouton submit
-  const handleOnSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post(
-        "https://finance-backend-mt66.onrender.com/user/",
-        {
-          ...inputValue,
-        },
-        { withCredentials: true }
-      );
+      const response = await axios.post('http://localhost:3000/api/user/register', formData);
 
-      const { success, message } = data;
-
-      if(success){
-        console.log("connecter");
-        handleSuccess(message);
-        setTimeout(() => {
-          navigate("/login");
-        }, 1000);
+      if (response.data.success) {
+        console.log('Inscription réussie');
+        // Redirection vers une page de connexion par exemple
+        // history.push('/login');
       } else {
-        handleError(message);
+        // L'inscription a échoué, afficher un message d'erreur.
+        console.error('Erreur lors de l\'inscription:', response.data.message);
       }
     } catch (error) {
-      console.log(error);
+      console.error('Erreur réseau lors de l\'inscription:', error, formData);
     }
-    setInputValue({
-      ...inputValue,
-      username: "",
-      email: "",
-      password: "",
-      contact: "",
-    });
   };
-
   return (
    <body className='inscrireBody'>
        <div className='inscription'>
       <h1>Inscription</h1>
       <Link to={"/login"} className='inscrireLink'><p>Deja un compte ? se connecter ici</p></Link>
-      <form onSubmit={handleOnSubmit}>
+      <form onSubmit={handleSubmit}>
         <div>
         <label>Nom & Prenom</label>
           <input
             type="text"
             name="username"
-            value={username}
+            value={formData.username}
             placeholder="Entrer Votre Nom"
-            onChange={handleOnChange}
+            onChange={handleChange}
             required
           />
           <label>Email</label>
           <input
             type="email"
             name="email"
-            value={email}
+            value={formData.email}
             placeholder="Entrer Votre Email"
-            onChange={handleOnChange}
+            onChange={handleChange}
             required
           />
           <label>Numero</label>
           <input
             type="number"
             name="contact"
-            value={contact}
+            value={formData.contact}
             placeholder="ENtrer Votre Numero de contact"
-            onChange={handleOnChange}
+            onChange={handleChange}
             required
           />
         </div>
@@ -108,9 +75,9 @@ function Register() {
           <input
            type="password"
            name="password"
-           value={password}
+           value={formData.password}
            placeholder="Enter your password"
-           onChange={handleOnChange}
+           onChange={handleChange}
             required
           />
         </div>
